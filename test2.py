8ba993d221616e948442bc8a28ee8c846c1db05a
petkapiter <petka@protonmail.ch>
2022-07-13 11:59:07 +0300

    for with if

--- test2.py
+++ test2.py
@@ -1,7 +1,13 @@
-for i in range(10):
-    print(f'interface FastEthernet0/{i}')
+access_template = ['switchport mode access',
+                    'switchport access vlan',
+                    'spanning-tree portfast',
+                    'spanning-tree bpduguard enable']
 
-vlans = [10, 20, 30, 40, 50, 60, 70,]
-for vlan in vlans:
-    print(f'vlan {vlan}')
-    print(f' name VLAN_{vlan}')
+access = {'0/12': 10, '0/14': 11, '0/16': 17, '0/17': 150}
+for intf, vlan in access.items():
+    print('interface Fa' +intf)
+    for command in access_template:
+        if command.endswith('access vlan'):
+            print(' {} {}'.format(command,vlan))
+        else:
+            print(' {}'.format(command))
