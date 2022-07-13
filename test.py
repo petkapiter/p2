582a171d04b0cc414736020e2021d76457bc25eb
petkapiter <petka@protonmail.ch>
2022-07-13 13:25:53 +0300

    while else

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-username = input('Введите имя пользователя: ')
-password = input('Введите парлоль: ')
-
-password_correct = False
-
-while not password_correct:
-    if len(password) < 8:
-        print('Пароль слишком короткий\n')
-        password = input('Введите парлоль: ')
-    else:
-        print(f'Пароль для пользователя {username} установлен')
-        password_correct = True
+i = 0
+while i < 5 :
+    print(i)
+    i += 1
+else:
+    print('Конец')
